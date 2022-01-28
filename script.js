65f7a12223b19716e0d17f384e604e0c7ba5c89a
Ainura Suerkulova <ainura.suerkulova.ss@gmail.com>
2022-01-28 20:33:49 +0600

    Add script.js task-1

---
+++ script.js
@@ -0,0 +1,44 @@
+//Task-1
+
+let userName = " <h1> Ainura welcome to Modul 2 ";
+document.write(userName);
+
+//Task-2
+
+/*let userName = "<h1> Ainura 'Ainura'<br>"; // использовала примитивные типы данных.
+document.write(userName);
+
+userName = 10;
+document.write(userName);*/
+
+//Task-3
+
+/*let userName = prompt("Ваше Имя ", "Ainura");
+let userEmail = prompt("Ваше почта", "ainura.suerkulova.ss@gmail.com");
+
+document.write("<h3> Ваш Имя:</h3>" + userName);
+document.write("<h3> Ваша почта:</h3>" + userEmail);*/
+
+//TasK-3
+
+/*let suV = prompt(" Введите количество покупок сувениров", 75);
+let beZ = prompt(" Введите количество покупок безделушек", 112);
+let all = suV * 75 + beZ * 112;
+document.write(`Общий вес вашей посылки: ${all} гр`);*/
+
+//Task-5
+
+/*let aa = prompt("Задайте значение для a");
+let bb = prompt("Задайте значение для b");
+
+let a = 5;
+let b = 2;
+document.write("<h1>a=" + a + "<br>");
+document.write("b=" + b + "<br>");
+document.write("a+b=" + (a + b) + "<br>");
+document.write("a-b=" + (a - b) + "<br>");
+document.write("a*b=" + a * b + "<br>");
+document.write("a/b=" + a / b + "<br>");
+document.write("a/b=" + Math.trunc(a / b) + "<br>");
+document.write("a*b=" + (a % b) + "<br>");
+document.write("a^b =" + a ** b + "<br>");*/
